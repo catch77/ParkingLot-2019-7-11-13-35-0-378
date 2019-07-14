5c8fb705e2c0b133eaa94d48598272888f7f6442
catch77 <1297056296@qq.com>
2019-07-14 15:40:56 +0800

    ADD: Story 1 AC2
    -the parking boy can park multiple cars

--- src/test/java/com/thoughtworks/tdd/CarTest.java
+++ src/test/java/com/thoughtworks/tdd/CarTest.java
@@ -6,10 +6,21 @@ import org.junit.jupiter.api.Test;
 public class CarTest {
 
     @Test
-    public void should_park_car_parking_boy() {
+    public void should_park_car_by_parking_boy() {
         Car car = new Car();
         ParkingBoy boy = new ParkingBoy();
         Ticket ticket = boy.park(car);
+        Assertions.assertSame(car, boy.fetch(ticket));
+    }
+
+    @Test
+    public void should_park_multiple_cars_by_parking_boy() {
+        Car car = new Car();
+        Car car1 = new Car();
+        ParkingBoy boy = new ParkingBoy();
+        Ticket ticket = boy.park(car);
+        Ticket ticket1 = boy.park(car1);
+        Assertions.assertSame(car1, boy.fetch(ticket1));
         Assertions.assertSame(car, boy.fetch(ticket));
     }
 }
